15362c0f8b8d7f8ffd4b3d814fa93b77c1525df2
Sharanjit Sandhu <sharanjitsandhu@amarbirs-mbp.lan>
2019-06-03 19:07:19 -0700

    Upgraded axios library

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,9 +1,83 @@
 import React, { Component } from "react";
+import axios from "axios";
 
-class SignUp extends Component {
+class Login extends Component {
+  state = {
+    username: "",
+    password: "",
+    department: ""
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    axios
+      .post("http://localhost:5000/api/register", this.state)
+      .then(res => {
+        localStorage.setItem("jwt", res.data.token);
+        this.props.history.push("/login");
+      })
+      .catch(err => {
+        console.log("Error", err);
+      });
+    this.setState({
+      username: "",
+      password: "",
+      department: ""
+    });
+  };
+
   render() {
-    return <div>SignUp</div>;
+    return (
+      <div>
+        <h2>Register</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="uname">
+            <b>Username</b>
+          </label>
+          <input
+            type="text"
+            placeholder="Enter Username"
+            name="username"
+            onChange={this.handleChange}
+            value={this.state.username}
+            required
+          />
+
+          <label htmlFor="pwd">
+            <b>Password</b>
+          </label>
+          <input
+            type="password"
+            placeholder="Enter Password"
+            name="password"
+            onChange={this.handleChange}
+            value={this.state.password}
+            required
+          />
+
+          <label htmlFor="dpt">
+            <b>Department</b>
+          </label>
+          <input
+            type="text"
+            placeholder="Enter Department"
+            name="department"
+            onChange={this.handleChange}
+            value={this.state.department}
+            required
+          />
+
+          <button type="submit">Sign Up</button>
+        </form>
+      </div>
+    );
   }
 }
 
-export default SignUp;
+export default Login;
